7a13b5658e657bd59f4672436d1c56992f5beb2b
Ankit Kulshreshtha <connect@ankitkul.in>
2013-05-05 10:07:19 -0700

    Tweet Sentiment Score (Prob 2) : WIP

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,34 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
 
 def lines(fp):
-    print str(len(fp.readlines()))
+    return fp.readlines()
 
+def processSenti(fp):
+	dictSenti = {}
+	for item in lines(fp):
+		sentence = item.split('\t')
+		dictSenti[sentence[0]] = sentence[1].rstrip()
+	return dictSenti
+
+def processTweets(fp, senti):
+    for item in lines(fp):
+        tweet = json.loads(item)["text"]
+        sentiScore = 0
+        for word in tweet.split(' '):
+            if word.lower() in senti:
+                sentiScore += int(senti[word.lower()])     
+        print tweet, sentiScore
+    
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    #hw()
+    senti = processSenti(sent_file)
+    processTweets(tweet_file, senti)
 
 if __name__ == '__main__':
     main()
